aa7f302c64f18438e538bb79b2e0fb3564af982e
jflh <55768308+jflh@users.noreply.github.com>
2019-11-04 16:57:42 -0330

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.stream.*;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -31,6 +32,7 @@ public class Board extends JFrame implements ActionListener {
 	            @Override
 	            public void mouseEntered(MouseEvent me) {
 	            	Board.this.hover(GridSquare.this);
+	            	//System.out.println(GridSquare.this.toString());
 	            }
 	            
 	            public void mouseExited(MouseEvent me) {
@@ -61,6 +63,7 @@ public class Board extends JFrame implements ActionListener {
 		public void setActive(String colour) {
 			if(!this.active) {
 				active = true;
+				this.colour = colour;
 				this.setBackground(Color.decode(colour));
 			}
 		}
@@ -87,6 +90,10 @@ public class Board extends JFrame implements ActionListener {
 			this.setBackground(Color.decode(colour));
 		}
 		
+		public String toString() {
+			return ("("+x+","+y+")" + "\nActive: " + active + "\nColour: " + colour);
+		}
+		
 	}
 	
 	private JPanel gridPanel = new JPanel();
@@ -104,7 +111,7 @@ public class Board extends JFrame implements ActionListener {
 		boardGrid = new GridSquare[row][col];
 		hoverQueue = new LinkedList<GridSquare>();
 		
-		selectedPiece = new Piece(7, "#25BE00"); // For testing
+		selectedPiece = new Piece(18, "#25BE00"); // For testing
 				
 		for (int i = 0; i < row; i++) { 
 			for (int j = 0; j < col; j++) { 
@@ -120,6 +127,7 @@ public class Board extends JFrame implements ActionListener {
 		 gridPanel.setLayout(new GridLayout(row, col));
 		 add(gridPanel);
 		 setSize(1920/2, 1080/2);
+		 setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
 		 setBounds();
 		 setVisible(true);		 
 	}
@@ -182,15 +190,11 @@ public class Board extends JFrame implements ActionListener {
 		while(valid && !allChecked) {
 			for(Point c : p.piecePath()) {
 				// Check if there are any adjacent active squares - Not valid if there are
+				if(boardGrid[(int) (origin.x+c.getX())][(int) (origin.y+c.getY())].active) {valid=false;}
 				if(this.squareAdjacent(boardGrid[(int) (origin.x+c.getX())][(int) (origin.y+c.getY())], p.getColour())) {valid=false;}
 				// Check that there is an active piece touching a corner - Needs to be at least one			
 				if(!toCorner) {
 					if(this.touchingCorner(boardGrid[(int) (origin.x+c.getX())][(int) (origin.y+c.getY())], p.getColour())) {toCorner = true;}
-					// Square is in corner of grid - Must be first turn of game
-//					else if((int)(origin.x+c.getX())==0 && (int)(origin.y+c.getY()) == 0 ||
-//							(int)(origin.x+c.getX())==row-2 && (int)(origin.y+c.getY()) == 0 ||
-//							(int)(origin.x+c.getX())==0 && (int)(origin.y+c.getY()) == col-2 ||
-//							(int)(origin.x+c.getX())==row-2 && (int)(origin.y+c.getY()) == col-2) {toCorner = true;}
 				}
 			}
 			allChecked = true;
@@ -202,7 +206,7 @@ public class Board extends JFrame implements ActionListener {
 		if(boardGrid[centre.x+1][centre.y].active && boardGrid[centre.x+1][centre.y].colour.equals(colour)) {return true;}
 		else if(boardGrid[centre.x][centre.y+1].active && boardGrid[centre.x][centre.y+1].colour.equals(colour)) {return true;}
 		else if(boardGrid[centre.x-1][centre.y].active && boardGrid[centre.x-1][centre.y].colour.equals(colour)) {return true;}
-		else if(boardGrid[centre.x+1][centre.y-1].active && boardGrid[centre.x][centre.y-1].colour.equals(colour)) {return true;}
+		else if(boardGrid[centre.x][centre.y-1].active && boardGrid[centre.x][centre.y-1].colour.equals(colour)) {return true;}
 		return false;
 	}
 	
@@ -220,6 +224,14 @@ public class Board extends JFrame implements ActionListener {
 	
 	public void actionPerformed(ActionEvent e){
 		//Probably not needed
-}
+	}
+	
+	public String toString() {
+		String grid = "";
+		for (int i=0; i<row; i++) {
+			grid = grid + "[" + Arrays.stream(boardGrid[i]).map(gs -> gs.colour).reduce((str1, str2) -> str1 + "," + str2) + "]\n";
+		}
+		return grid;
+	}
 }
 
